95a59a2a2be5eb258f3eb84ec2e9a2d6550b8aa5
Nikita <Nikita@Nikita-Note>
2012-04-01 20:23:37 +0400

    ????.

---
+++ www/js/main.js
@@ -0,0 +1,15 @@
+﻿$(function(){
+	/* Процентные отступы у лого */
+	var header = $('.l-header'),
+		logo = $('.b-logo', header);
+	
+	setHeaderPadding();
+	$(window).resize(setHeaderPadding);
+	
+	function setHeaderPadding(){
+		header.css({
+			'padding-top': Math.round(logo.height() * 0.33),
+			'padding-bottom': Math.round(logo.height() * 0.37)
+		});
+	}
+});
